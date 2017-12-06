2293667fed6ee5783214f70192519d01769b7a66
ivonaj <ivonaj1@gmail.com>
2017-12-06 23:01:08 +0100

    add publish in callback and time-hack

--- p3dx_config/script/republish_velocity.py
+++ p3dx_config/script/republish_velocity.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
 import rospy
+import time
 from geometry_msgs.msg import Twist
 
 
 storeVel=Twist()
 def velocity_callback(msg):
     global storeVel
+    global pub
     storeVel.linear=msg.linear
     storeVel.angular=msg.angular
+    pub.publish(msg)
 
 
 if __name__=='__main__':
     rospy.init_node('republish_velocity')
 
-    sub=rospy.Subscriber("cmd_vel_orig",Twist,velocity_callback)
     pub=rospy.Publisher("cmd_vel", Twist, queue_size=1)
-    rate=rospy.Rate(10)
+    sub=rospy.Subscriber("cmd_vel_orig",Twist,velocity_callback)
 
     while not rospy.is_shutdown():
         if storeVel:
             pub.publish(storeVel)
-            rate.sleep()
+            time.sleep(1/10.)
